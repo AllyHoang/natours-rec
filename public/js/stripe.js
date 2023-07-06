aca9fc67c90d666c21954c30aa66ec3d63fc3b95
AllyHoang <hdtanh2404@gmail.com>
2023-07-06 22:15:50 +0700

    Initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,21 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = stripe('pk_test_51NQtblAl0q3T8XfkyAuofozbUwVwYuisIGANU9q8FOhXeV71aml5vTA7DYMnaqd4qiR0mvp5HIcvnWDBbIWlvDHq00LMq6cQ5N');
+
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
